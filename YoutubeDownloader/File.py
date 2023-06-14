76f30650802efa82e8a55eebe29a4a5c0063b6b6
artick788 <60552965+artick788@users.noreply.github.com>
2023-06-14 22:43:47 +0200

    added Key and BPM Fields

--- YoutubeDownloader/File.py
+++ YoutubeDownloader/File.py
@@ -15,10 +15,18 @@ class MusicFile:
         self.artist: str = ""
         self.songname: str = ""
         self.album: str = ""
+        self.genre: str = ""
+        self.bpm: str = ""
+        self.key: str = ""
         self.format: int = FileFormat.MP3
 
     def save_tags(self, file_path: str):
         f = taglib.File(file_path)
         f.tags["ARTIST"] = self.artist
         f.tags["TITLE"] = self.songname
+        f.tags["ALBUM"] = self.album
+        f.tags["GENRE"] = self.genre
+        f.tags["BPM"] = self.bpm
+        f.tags["TKEY"] = self.key
+
         f.save()
--- YoutubeDownloaderGUI/YtDownloadPanel.py
+++ YoutubeDownloaderGUI/YtDownloadPanel.py
@@ -9,16 +9,18 @@ class DownloadPanel:
         self.url = ""
         self.song = mf.MusicFile()
 
-        self.reset_url: bool = True
         self.reset_artist: bool = True
         self.reset_songname: bool = True
         self.reset_album: bool = True
+        self.reset_genre: bool = True
+        self.reset_bpm: bool = True
+        self.reset_key: bool = True
 
     def draw(self):
         imgui.begin("Download", False, imgui.WINDOW_NO_RESIZE | imgui.WINDOW_NO_MOVE | imgui.WINDOW_NO_TITLE_BAR)
         self.url = imgui.input_text("URL", self.url, 256)[1]
-        imgui.same_line()
-        self.reset_url = imgui.checkbox("Reset URL", self.reset_url)[1]
+
+        imgui.text("Tags")
         self.song.artist = imgui.input_text("Artist", self.song.artist, 256)[1]
         imgui.same_line()
         self.reset_artist = imgui.checkbox("Reset Artist", self.reset_artist)[1]
@@ -28,6 +30,15 @@ class DownloadPanel:
         self.song.album = imgui.input_text("Album", self.song.album, 256)[1]
         imgui.same_line()
         self.reset_album = imgui.checkbox("Reset Album", self.reset_album)[1]
+        self.song.genre = imgui.input_text("Genre", self.song.genre, 256)[1]
+        imgui.same_line()
+        self.reset_genre = imgui.checkbox("Reset Genre", self.reset_genre)[1]
+        self.song.bpm = imgui.input_text("BPM", self.song.bpm, 256)[1]
+        imgui.same_line()
+        self.reset_bpm = imgui.checkbox("Reset BPM", self.reset_bpm)[1]
+        self.song.key = imgui.input_text("Key", self.song.key, 256)[1]
+        imgui.same_line()
+        self.reset_key = imgui.checkbox("Reset Key", self.reset_key)[1]
 
         if imgui.button("Download", 100, 20):
             self.factory.download_from_youtube(self.url, self.song)
@@ -46,11 +57,16 @@ class DownloadPanel:
         return return_state
 
     def clear_values(self):
-        if self.reset_url:
-            self.url = ""
+        self.url = ""
         if self.reset_artist:
             self.song.artist = ""
         if self.reset_songname:
             self.song.songname = ""
         if self.reset_album:
             self.song.album = ""
+        if self.reset_genre:
+            self.song.genre = ""
+        if self.reset_bpm:
+            self.song.bpm = ""
+        if self.reset_key:
+            self.song.key = ""
